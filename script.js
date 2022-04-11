443a6c58e927d51d2a85080df29241f6b3b9792c
Chikoti123 <chikotiswetha@gmail.com>
2022-04-11 14:00:43 +1000

    changes added

--- script.js
+++ script.js
@@ -149,7 +149,32 @@ function loadTrack(track_index) {
         // percentage of the volume slider set
         curr_track.volume = volume_slider.value / 100;
         }
-        
+        function seekUpdate() {
+            let seekPosition = 0;
+            
+            // Check if the current track duration is a legible number
+            if (!isNaN(curr_track.duration)) {
+              seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+              seek_slider.value = seekPosition;
+        // duration of track
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+      
+        // Add a zero to the single digit time values
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+      
+        // Display the updated duration
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+      
+    
+            }
+        }
        
         // Load e tracklist
 loadTrack(track_index);
